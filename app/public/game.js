ac0dd763c54c026b8e8f24b98419b66d04a2dd34
saker <sakertooth@gmail.com>
2026-07-27 03:40:25 -0400

    Add toggle functionality for play/pause buttons

--- app/public/game.js
+++ app/public/game.js
@@ -1,4 +1,6 @@
-const PATTERN_STEP_RESOLUTION = 16
+const PATTERN_STEP_RESOLUTION = 16;
+const PLAY_BUTTON_ICON_PATH = "M3 2l11 6-11 6V2z";
+const PAUSE_BUTTON_ICON_PATH = "M3 2h3.5v12H3V2zm6.5 0H13v12H9.5V2z";
 
 let game = {
     pattern: [
@@ -10,6 +12,10 @@ let game = {
     timeSigNumerator: 4,
     timeSigDenominator: 4,
     beatsPerMinute: 140
+}
+
+let context = {
+    playing: false
 }
 
 function calculateNumSteps() {
@@ -54,11 +60,28 @@ function renderSequencer() {
     });
 }
 
-const patternStepResolution = 16;
 const numSteps = calculateNumSteps();
-
 for (let track of game.pattern) {
     track.steps = Array(numSteps).fill(false);
 }
 
 renderSequencer();
+
+let playButton = document.getElementById("play-button");
+let playIconPath = document.querySelector("#play-icon path");
+
+playButton.addEventListener("click", () => {
+    context.playing = !context.playing;
+    if (context.playing) {
+        playIconPath.setAttribute("d", PLAY_BUTTON_ICON_PATH);
+    }
+    else {
+        playIconPath.setAttribute("d", PAUSE_BUTTON_ICON_PATH);
+    }
+});
+
+document.addEventListener("keyup", (e) => {
+    if (e.code === "Space") {
+        playButton.click();
+    }
+});

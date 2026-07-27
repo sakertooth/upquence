299bc1ff88f7c858450d7c2ded309d570cc128d8
saker <sakertooth@gmail.com>
2026-07-27 04:32:37 -0400

    Add transport functionality using Tone.js

--- app/public/game.js
+++ app/public/game.js
@@ -1,6 +1,4 @@
 const PATTERN_STEP_RESOLUTION = 16;
-const PLAY_BUTTON_ICON_PATH = "M3 2l11 6-11 6V2z";
-const PAUSE_BUTTON_ICON_PATH = "M3 2h3.5v12H3V2zm6.5 0H13v12H9.5V2z";
 
 let game = {
     pattern: [
@@ -12,10 +10,6 @@ let game = {
     timeSigNumerator: 4,
     timeSigDenominator: 4,
     beatsPerMinute: 140
-}
-
-let context = {
-    playing: false
 }
 
 function calculateNumSteps() {
@@ -60,28 +54,48 @@ function renderSequencer() {
     });
 }
 
-const numSteps = calculateNumSteps();
-for (let track of game.pattern) {
-    track.steps = Array(numSteps).fill(false);
+function setupPlayButton() {
+    const PLAY_BUTTON_ICON_PATH = "M3 2l11 6-11 6V2z";
+    const PAUSE_BUTTON_ICON_PATH = "M3 2h3.5v12H3V2zm6.5 0H13v12H9.5V2z";
+
+    let playButton = document.getElementById("play-button");
+    let playIconPath = document.querySelector("#play-icon path");
+
+    playButton.addEventListener("click", async () => {
+        if (Tone.Transport.state === "stopped" || Tone.Transport.state === "paused") {
+            await Tone.start();
+            Tone.Transport.start();
+            playIconPath.setAttribute("d", PAUSE_BUTTON_ICON_PATH);
+        }
+        else {
+            Tone.Transport.pause();
+            playIconPath.setAttribute("d", PLAY_BUTTON_ICON_PATH);
+        }
+    });
+
+    document.addEventListener("keyup", (e) => {
+        if (e.code === "Space") {
+            playButton.click();
+        }
+    });
 }
 
+function initalize() {
+    const numSteps = calculateNumSteps();
+    for (let track of game.pattern) {
+        track.steps = Array(numSteps).fill(false);
+    }
+
+    Tone.Transport.timeSignature = [game.timeSigNumerator, game.timeSigDenominator];
+    Tone.Transport.bpm.value = game.beatsPerMinute;
+    Tone.Transport.loop = true;
+    Tone.Transport.setLoopPoints(0, "1m");
+
+    Tone.Transport.scheduleRepeat((time) => {
+        console.log(Tone.Transport.position);
+    }, "16n");
+}
+
+initalize();
+setupPlayButton();
 renderSequencer();
-
-let playButton = document.getElementById("play-button");
-let playIconPath = document.querySelector("#play-icon path");
-
-playButton.addEventListener("click", () => {
-    context.playing = !context.playing;
-    if (context.playing) {
-        playIconPath.setAttribute("d", PLAY_BUTTON_ICON_PATH);
-    }
-    else {
-        playIconPath.setAttribute("d", PAUSE_BUTTON_ICON_PATH);
-    }
-});
-
-document.addEventListener("keyup", (e) => {
-    if (e.code === "Space") {
-        playButton.click();
-    }
-});

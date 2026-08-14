51b4e24ea1bb2dfe0810483c527f9732a46be4ea
Sotonye Atemie <sakertooth@gmail.com>
2026-08-14 11:40:35 -0400

    Use parseFloat when converting volume and panning sliders to numbers

--- app/public/scripts/view/sequencer.js
+++ app/public/scripts/view/sequencer.js
@@ -53,7 +53,7 @@ function render() {
         Game.changeVolume(trackAudioIndex, trackVolumeSlider.defaultValue);
 
         trackVolumeSlider.addEventListener("input", () => {
-            Game.changeVolume(trackAudioIndex, Number.parseInt(trackVolumeSlider.value));
+            Game.changeVolume(trackAudioIndex, Number.parseFloat(trackVolumeSlider.value));
             trackVolumeDisplay.textContent = trackVolumeSlider.value + "dB";
         });
 
@@ -70,7 +70,7 @@ function render() {
         trackPanDisplay.textContent = trackPanInput.value;
 
         trackPanInput.addEventListener("input", () => {
-            Game.changePanning(trackAudioIndex, Number.parseInt(trackPanInput.value));
+            Game.changePanning(trackAudioIndex, Number.parseFloat(trackPanInput.value));
             trackPanDisplay.textContent = trackPanInput.value;
         });
 

3cbaec0227a10493f142de9c083b9e122995f0a8
Sotonye Atemie <sakertooth@gmail.com>
2026-08-14 12:45:32 -0400

    Add setup.js for setting up database with initial levels

---
+++ app/setup.js
@@ -0,0 +1,12 @@
+const { Pool } = require("pg");
+const pool = new Pool(require("../env.json"));
+
+const easyLevel = require("levels/easy.json");
+const mediumLevel = require("levels/medium.json");
+const hardLevel = require("levels/hard.json");
+
+(() => {
+    await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [easyLevel]);
+    await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [mediumLevel]);
+    await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [hardLevel]);
+})();

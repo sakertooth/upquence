9099cbc07b5088c31577a7defd6d4f979c046aeb
Sotonye Atemie <sakertooth@gmail.com>
2026-08-14 19:20:44 -0400

    Add async

--- app/setup.js
+++ app/setup.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 const pool = new Pool(require("../env.json"));
 
-const easyLevel = require("levels/easy.json");
-const mediumLevel = require("levels/medium.json");
-const hardLevel = require("levels/hard.json");
+const easyLevel = require("./levels/easy.json");
+const mediumLevel = require("./levels/medium.json");
+const hardLevel = require("./levels/hard.json");
 
-(() => {
+(async () => {
     await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [easyLevel]);
     await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [mediumLevel]);
     await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [hardLevel]);

e92536a7f6f41b9de7a3b7d8765b601c97295a57
Sotonye Atemie <sakertooth@gmail.com>
2026-08-14 19:29:10 -0400

    Fix hanging in setup.js

--- app/setup.js
+++ app/setup.js
@@ -1,12 +1,13 @@
-const { Pool } = require("pg");
-const pool = new Pool(require("../env.json"));
-
 const easyLevel = require("./levels/easy.json");
 const mediumLevel = require("./levels/medium.json");
 const hardLevel = require("./levels/hard.json");
+
+const { Pool } = require("pg");
+const pool = new Pool(require("../env.json"));
 
 (async () => {
     await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [easyLevel]);
     await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [mediumLevel]);
     await pool.query(`INSERT INTO levels (data) VALUES ($1)`, [hardLevel]);
+    await pool.end();
 })();
